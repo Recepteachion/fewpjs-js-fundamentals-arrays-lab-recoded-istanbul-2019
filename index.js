ede4996242dda6288c237da840f30ccefdc1719a
Recep Öztürk <turkishteacher23@gmail.com>
2020-01-13 18:14:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const append = ["Milo","Otis","Garfeld"]
 const prepend = ["Milo","Otis","Garfield"]
 const removeLast = ["Milo","Otis","Garfield"]
 const removeFirst = ["Milo","Otis","Garfield"]
+
+append.push("Odie")
